de71fdc43494225a9aefc172b4162aa49dcbd2b4
Joonas Westlin <joonas.westlin@onsight.fi>
2017-06-05 15:01:21 +0300

    Implemented search page.

---
+++ Contoso.Events/Contoso.Events.Models/EventSearchResult.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Contoso.Events.Models
+{
+    public class EventSearchResult
+    {
+        [JsonProperty("@search.score")]
+        public double SearchScore { get; set; }
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        public string EventKey { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}

b053c701999d7c94221a0959b7ddf5d128734ae2
Joonas Westlin <joonas.westlin@onsight.fi>
2017-06-06 13:51:27 +0300

    Cleaned up Startup.Auth.cs

--- Contoso.Events/Contoso.Events.Web/App_Start/Startup.Auth.cs
+++ Contoso.Events/Contoso.Events.Web/App_Start/Startup.Auth.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.IdentityModel.Tokens;
-using System.Linq;
+﻿using System.Configuration;
 using Microsoft.Owin.Security;
-using Microsoft.Owin.Security.ActiveDirectory;
 using Owin;
 using Microsoft.Owin.Security.OpenIdConnect;
 using Microsoft.Owin.Security.Cookies;
@@ -13,24 +8,11 @@ namespace Contoso.Events.Web
 {
     public partial class Startup
     {
-        // For more information on configuring authentication, please visit https://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
-
-            //app.UseWindowsAzureActiveDirectoryBearerAuthentication(
-            //    new WindowsAzureActiveDirectoryBearerAuthenticationOptions
-            //    {
-            //        Tenant = ConfigurationManager.AppSettings["ida:Tenant"],
-            //        TokenValidationParameters = new TokenValidationParameters
-            //        {
-            //            ValidAudience = ConfigurationManager.AppSettings["ida:Audience"]
-            //        },
-            //    });
             app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
 
-            app.UseCookieAuthentication(new CookieAuthenticationOptions
-            {
-            });
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
 
             app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
             {
